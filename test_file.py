9692bc96320b2a65befa19e6a954ccb016e2846d
pa.kulyasov <pa.kulyasov@tensor.ru>
2021-11-03 14:31:20 +0300

    changed test_file.py

--- test_file.py
+++ test_file.py
@@ -1,74 +1,121 @@
 from tkinter import *
-from PIL import Image, ImageTk
-import screenshoter as ss
+import action_buttons as ab
+import gc
 
-class OpenScreen(Frame):
+# наработки по action_button
+
+class AddDel(Frame):
+    new_list = []
 
     def __init__(self, parent):
-        Frame.__init__(self, parent)
+        super().__init__()
         self.parent = parent
-        self.start_x, self.start_y, self.x1, self.y1 = 0, 0, 0, 0
-        self.imageSize()
         self.initUI()
-        self.process_movements()
-
-    def quit(self, arg1):
-        self.parent.destroy()
-
-    def paint(self, event):
-        self.x1, self.y1 = event.x, event.y
-
-    def start_points(self, event):
-        self.start_x, self.start_y = event.x, event.y
-
-    def process_movements(self):
-        self.canvas.delete("aaa")
-        self.canvas.focus_set()
-        self.canvas.bind("<B1-Motion>", self.paint)
-        self.canvas.bind("<Button-1>", self.start_points)
-        self.item = self.canvas.create_rectangle(self.start_x, self.start_y, self.x1, self.y1, tag="aaa",
-                                                 outline="black", dash=(1, 1))
-        self.after(10, self.process_movements)
-
-    def imageSize(self):
-        try:
-            self.img = Image.open("temp_screen.png")
-            self.fillimg = self.img.putalpha(125)
-            self.screen = ImageTk.PhotoImage(self.img)
-        except IOError:
-            print("Возникла ошибка во время открытия изображения!")
-        self.wx, self.hy = self.img.size
+        self.a = ab.Counter()
+        # self.call_class()
+        
+    def increment_count(self):
+        self.a.inctrement_count
 
     def initUI(self):
-        self.canvas = Canvas(self.parent, width=self.wx, height=self.hy, highlightthickness=0)
-        image = self.canvas.create_image(0, 0, anchor="nw", image=self.screen)
-        self.canvas.grid(column=0, row=0)
-        self.canvas.bind("<Escape>", quit)
+        # self.pack(fill=BOTH, expand=True)
+        btn_add = Button(self.parent, text="Add buttons", command=ab.Panel)
+        btn_add.pack()
+
+        btn_print = Button(self.parent, text="все объекты", command=self.all_obj)
+        btn_print.pack()
+
+    def all_obj(self):
+        # for obj in gc.get_objects():        
+        #     if isinstance(obj, ab.Panel):
+        #         print(obj)
+        for inst in ab.Panel.get_instances():
+            print(inst)
 
 
-    def setGeometry(self):
-        self.parent.geometry(f"{self.wx}x{self.hy}")
+root = Tk()
+root.geometry("+500+300")
+app = AddDel(root)
+root.mainloop()
+
+
+# from tkinter import *
+# from PIL import Image, ImageTk
+# import screenshoter as ss
+
+# class OpenScreen(Frame):
+
+#     def __init__(self, parent):
+#         Frame.__init__(self, parent)
+#         self.parent = parent
+#         self.start_x, self.start_y, self.x1, self.y1 = 0, 0, 0, 0
+#         self.imageSize()
+#         self.initUI()
+#         self.process_movements()
+
+#     def quit(self, arg1):
+#         self.parent.destroy()
+
+#     def paint(self, event):
+#         self.x1, self.y1 = event.x, event.y
+
+#     def start_points(self, event):
+#         self.start_x, self.start_y = event.x, event.y
+
+#     def process_movements(self):
+#         self.canvas.delete("aaa")
+#         self.canvas.focus_set()
+#         self.canvas.bind("<B1-Motion>", self.paint)
+#         self.canvas.bind("<Button-1>", self.start_points)
+#         self.item = self.canvas.create_rectangle(self.start_x, self.start_y, self.x1, self.y1, tag="aaa",
+#                                                  outline="black", dash=(1, 1))
+#         self.after(10, self.process_movements)
+
+#     def imageSize(self):
+#         try:
+#             self.img = Image.open("temp_screen.png")
+#             self.fillimg = self.img.putalpha(125)
+#             self.screen = ImageTk.PhotoImage(self.img)
+#         except IOError:
+#             print("Возникла ошибка во время открытия изображения!")
+#         self.wx, self.hy = self.img.size
+
+#     def initUI(self):
+#         self.canvas = Canvas(
+#             self.parent, 
+#             width=self.wx, 
+#             height=self.hy, 
+#             highlightthickness=0
+#             )
+#         image = self.canvas.create_image(0, 0, anchor="nw", image=self.screen)
+#         self.canvas.grid(column=0, row=0)
+#         self.canvas.bind("<Escape>", quit)
+
+
+#     def setGeometry(self):
+#         self.parent.geometry(f"{self.wx}x{self.hy}")
 
 
 # https://note.nkmk.me/en/python-pillow-putalpha/
 # https://pillow.readthedocs.io/en/stable/
 # https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/create_rectangle.html
 # https://pythonru.com/uroki/canvas-3-tkinter-20
+# https://tkdocs.com/tutorial/windows.html закрытие верхних окон
 
 # Нужно чтобы два изображения открывались в двух слоях, затуманенный снизу. 
 # Получается, что каждый цикл идет рисование картинки в виде прямоугольника 
 # с отступами равными sarts_point
 
-def main():
-    root = Tk()
-    # root.geometry("3840x1080")
-    app = OpenScreen(root)
-    root.overrideredirect(True)
-    root.mainloop()
+# def main():
+#     root = Tk()
+#     # root.geometry("3840x1080")
+#     app = OpenScreen(root)
+#     root.overrideredirect(True)
+#     root.mainloop()
     
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
 
 # ----------------------------------------------------
