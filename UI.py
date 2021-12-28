71c2c52ec25e26188cf0842b45f218ae7e892601
pa.kulyasov <pa.kulyasov@tensor.ru>
2021-12-28 10:19:00 +0300

    changed UI and added comments to action steps

--- UI.py
+++ UI.py
@@ -8,13 +8,15 @@ import action_buttons as ab
 
 class Example(Frame):
     row_count = 1
-    row_dict = {}
+    # row_dict = {} # Словарь думаю не нужен, т.к. есть списко row_list актуальных позиций панелей из которого по порядку тянем действия
     row_list = [0]
     button_up = [0]
     button_down = [0]
 
     def __init__(self, parent):
         Frame.__init__(self, parent)   
+        self.menu_frame = Frame(master=parent)
+        self.menu_frame.grid(column=1, row=0, sticky=W, pady=(0, 20))
         self.parent = parent
         # self.initUI()
         self.buttons()
@@ -108,57 +110,70 @@ class Example(Frame):
         if caller in self.button_down and position < (len(self.button_down) - 1):
             self.panels_down(position)
 
-    def all_obj(self):
-        temp_row_dict = self.row_dict.copy()
-        for act in temp_row_dict:
-            if act.state_delete == 0:
-                self.row_dict[act] = act.get_action()
-            else:
-                del self.row_dict[act]
+    # def all_obj(self):
+    #     """Словарь строк очищается от экземпляров удаленных панелей."""
+    #     temp_row_dict = self.row_dict.copy()
+    #     for act in temp_row_dict:
+    #         if act.state_delete == 0:
+    #             self.row_dict[act] = act.get_action()
+    #         else:
+    #             del self.row_dict[act]
 
     def initUI(self):
         self.parent.title("Simple")
         # self.pack(fill=BOTH, expand=1)
 
     def print_dict(self):
-        temp_row_dict = self.row_dict.copy()
-        for act in temp_row_dict:
-            if act.state_delete == 0:
-                self.row_dict[act] = act.get_action()
-            else:
-                del self.row_dict[act]
-        print(self.row_dict)
+        # temp_row_dict = self.row_dict.copy()
+        # for act in temp_row_dict:
+        #     if act.state_delete == 0:
+        #         self.row_dict[act] = act.get_action()
+        #     else:
+        #         del self.row_dict[act]
+        print(self.row_list)
 
     def save_file(self):
-        self.all_obj()
-        # for act in self.row_dict:
-        #     self.row_dict[act] = act.get_action()
+        # self.all_obj()
         with open("test_save.py", "w", encoding="utf-8")as wf:
             text_import = actions.Actions().turn_on_lib()
             wf.write(text_import)
-            for act in self.row_dict.values():
-                wf.write(act)
+            for act in self.row_list[1:]:
+                wf.write(act.get_action())
 
     def buttons(self):
-        btn_add = Button(master=self.parent, text="Добавить", command=self.add_panel)
+        btn_add = Button(master=self.menu_frame, text="Добавить", command=self.add_panel)
         btn_add.grid(column=0, row=0)
 
-        btn_screenshot = Button(master=self.parent, text="Скриншот", command=ss.main)
-        btn_screenshot.grid(column=10, row=0)
+        btn_screenshot = Button(master=self.menu_frame, text="Скриншот", command=ss.main)
+        btn_screenshot.grid(column=1, row=0)
 
-        btn_settings = Button(master=self.parent, text="Настроить монитор", command=gc.getConfig().change_monitor_cfg())
-        btn_settings.grid(column=11, row=0)
+        btn_settings = Button(master=self.menu_frame, text="Настроить монитор", command=gc.getConfig().change_monitor_cfg())
+        btn_settings.grid(column=2, row=0)
 
-        btn_print = Button(master=self.parent, text="Print", command=self.print_dict)
-        btn_print.grid(column=12, row=0)
+        btn_print = Button(master=self.menu_frame, text="Print", command=self.print_dict)
+        btn_print.grid(column=3, row=0)
 
-        btn_save = Button(master=self.parent, text="Сохранить", command=self.save_file)
-        btn_save.grid(column=13, row=0)
+        btn_save = Button(master=self.menu_frame, text="Сохранить", command=self.save_file)
+        btn_save.grid(column=4, row=0)
 
 # Нужно доработать отображение(удаление) панели оберки в виде кнопок вверх и вниз
 
 # https://stackoverflow.com/questions/328851/printing-all-instances-of-a-class посмотреть все инстансы класса
-        
+
+# ПОРЯДОК ДОДЕЛКИ ПРИЛОЖЕНИЯ ДО СОСТОЯНИЯ РАБОЧЕГО!!! 
+# 1. Сделать кнопку сохранения файла в определенную директорию и с указанием названия 
+#   - https://pythonru.com/uroki/dialogovye-vsplyvajushhie-okna-tkinter-9 
+#   - https://younglinux.info/tkinter/dialogbox
+# 2. Допилить к панели, в виде флага включения, сохранение скриншота  после выполнения действия 
+#   - https://pythonru.com/uroki/polja-vybora-znachenij-tkinter-3 
+#   - https://younglinux.info/tkinter/variable
+# 3. Добавить действий из puautogui https://pyautogui.readthedocs.io/en/latest/
+# 4. Написать мини-программу для последовательного запуска файлов автотестов с возможностью выбора стенда для запуска и сверки с эталонами 
+# 5. Рефакторинг/оптимизация кода и добавление комментов к методам.
+
+
+
+
 # def main():
 if __name__ == '__main__':
     root = Tk()
